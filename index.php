6e2acea33a5e49ddeb87420dedd2da71f1184821
kevin-daroowalas <79782083+kevin-daroowalas@users.noreply.github.com>
2023-06-12 19:41:13 -0400

    index.php added
    Addition of file index.php to repository
    The background image added
    Text Restaurant Bombay Mahal added

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            Restaurant Bombay Mahal
+        </title>
+    </head>
+    <body>
+        <style>
+            body
+            {
+                background-image: url("photo/background.jpg");
+            }
+        </style>
+        <center>
+            <h1>Restaurant Bombay Mahal</h1>
+        </center>
+    </body>
+</html>
