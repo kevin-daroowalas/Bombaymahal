e571c3cd371e38c51a8e8bfbd2eb8323f89db9c5
kevin-daroowalas <79782083+kevin-daroowalas@users.noreply.github.com>
2023-06-15 15:48:07 -0400

    addition of navbar + img
    Addition of the navbar with its items being  Accueil, A propos de nous, menu, contact and English.
    
    Addition of Javascript and Bootstrap
    
    Addition of the image of the restaurant logo

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
         <title>
             Restaurant Bombay Mahal
         </title>
+           <meta charset="utf-8">
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+           <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+           <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+           <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
         <style>
@@ -11,8 +17,43 @@
                 background-image: url("photo/background.jpg");
             }
         </style>
+        <nav class=" navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+            <div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accueil
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">À propos de nous
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Menu
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">English
+                        </a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </nav>
+        
+        <div class="container-fluid" style="margin-top:80px">
+        
+        </div>
         <center>
-            <h1>Restaurant Bombay Mahal</h1>
+            <img src='photo/27.png' style='height:450px;width:550px'/>
         </center>
     </body>
 </html>
